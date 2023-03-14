448cb0f530a53edf0ecf344549fa601c4c15ab03
Robert Rogers <rerogers@gmail.com>
2023-03-14 14:30:49 -0500

    Updated Encapsulation practice

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -2,9 +2,23 @@
 {
     public class Program
     {
+        public static double askDeposit()
+        {
+            Console.WriteLine("How much would you like to deposit?");
+            double deposit = Convert.ToDouble(Console.ReadLine());
+            return deposit;
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // create a user with a BankAccount
+            BankAccount user = new BankAccount();
+
+            // ask for money to deposit
+            user.Deposit(Convert.ToDouble(askDeposit()));
+
+            // give balance
+            Console.WriteLine($"Your balance is currently:\n{user.GetBalance(), 0:C}");
         }
     }
 }
